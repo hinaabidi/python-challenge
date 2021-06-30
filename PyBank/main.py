7e12619ed877e0cd9a6a3be4d2f7b4c713789372
hinaabidi <83368871+hinaabidi@users.noreply.github.com>
2021-06-30 18:07:21 -0400

    PyBank Incomplete

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,21 +5,34 @@ with open('Resources/budget_data.csv') as csv_file:
     csv_reader = csv.reader(csv_file)
 
     months = 0
-    total = 0
+    total = 0.00
+    max_profit = 0.00
+    max_profit_date = ""
+    profit = 0.00
+    loss = 0.00
+    min_profit = 0.00
+    min_profit_date = ""
     next(csv_reader)
     
     for row in csv_reader:
         months += 1
         total += float(row[1])
+        if float(row[1]) > max_profit:
+            max_profit = float(row[1])
+            max_profit_date = str(row[0])
+        if float(row[1]) < min_profit:
+            min_profit = float(row[1])
+            min_profit_date = str(row[0])
+    
 
     output = (
-        '''  Financial Analysis
+    '''  Financial Analysis
     ----------------------------
     Total Months: %s
-    Total: $%s
+    Total: $%.2f
     Average  Change: $-2315.12
-    Greatest Increase in Profits: Feb-2012 ($1926159)
-    Greatest Decrease in Profits: Sep-2013 ($-2196167)'''
+    Greatest Increase in Profits: %s ($%.2f)
+    Greatest Decrease in Profits: %s ($%.2f)'''
     )
 
-    print(output % (months, total) )
+    print(output % (months, total, max_profit_date, max_profit, min_profit_date, min_profit ) )
