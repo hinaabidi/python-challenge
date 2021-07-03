f7cc9058b6a357571c9a2164a2f2a764ea6fc5cc
hinaabidi <83368871+hinaabidi@users.noreply.github.com>
2021-07-03 14:06:20 -0400

    PyPoll completed

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,16 +3,22 @@ import csv
 import os
 
 votes = {}
-with open('Resources/election_data.csv', mode='rU') as csv_file:
+with open('Resources/election_data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-
+    row_count = sum(1 for row in csv_reader)
+    csv_file.seek(0)
     total_votes = 0
     current_candidate_name = ""
     previous_candiate_name = ""
     vote_count = 0 
+    #Get the total number of rows
+    
     next(csv_reader)
     for n, row in enumerate(csv_reader):
+        #get the totla number of votes
         total_votes += 1
+        # increment the vote until the next candidate
+        vote_count = vote_count + 1
         current_candidate_name = row[2]
         if n > 0:
             # check if the canditate is different than the previous candidate
@@ -20,45 +26,44 @@ with open('Resources/election_data.csv', mode='rU') as csv_file:
                 # if this is the first time the candiate is added
                 if previous_candiate_name not in votes:
                     votes[previous_candiate_name] = vote_count
-                elif n == csv_reader.line_num:
-                   vote_count = vote_count + 1 
-                   votes[previous_candiate_name] = vote_count
+                elif n == ( row_count - 2 ):
+                   votes[previous_candiate_name] = votes[previous_candiate_name] + vote_count
                 else:
                     # if the candiate is alrady there, then add the vote
                     votes[previous_candiate_name] = votes[previous_candiate_name] + vote_count
                     # reset the vote count for the next candidate
                 vote_count = 0
         previous_candiate_name = current_candidate_name
-        # increment the vote until the next candidate
-        vote_count = vote_count + 1
-
-
+        
         
 
+list_of_canidates = ""
+highest_vote = 0
+percentage = 0.0000
+winner = ""
+for key in votes:
+    if int(votes[key]) > highest_vote:
+        highest_vote = int(votes[key])
+        winner = key
+    print(key, '->', votes[key])
+    percentage = ( float(votes[key] / total_votes)) * 100
+    list_of_canidates = list_of_canidates + "%s : %.4f%% (%d)\n" %(key,percentage, votes[key])
+    percentage = 0.0000
     
-
-
-
-
-
 
 output = (
 '''  Election Results
 -------------------------
 Total Votes: %d
 -------------------------
-Khan: 63.000%% (2218231)
-Correy: 20.000%% (704200)
-Li: 14.000%% (492940)
-O'Tooley: 3.000%% (105630)
+%s
 -------------------------
-Winner: Khan
+Winner: %s
 -------------------------
 '''
 )
-
-print(output % (total_votes) )
+print(output % (total_votes, list_of_canidates, winner) )
 
 file = open("analysis/results.txt", "w")
-file.write(output % (total_votes ))
+file.write(output % (total_votes, list_of_canidates, winner ))
 file.close()
